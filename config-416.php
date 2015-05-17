008fd1ba38f3077bcc3d940329bf9e52303eaa63
JCO <470521443@qq.com>
2015-05-17 20:17:35 +0800

    Signed-off-by: JCO <470521443@qq.com>

---
+++ config-416.php
@@ -0,0 +1,35 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "elife";
+
+// database password
+$db_pass   = "elife966733";
+
+// table prefix
+$prefix    = "main_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-05-08 09:11:31');
+
+?>
---
+++ config.php
@@ -0,0 +1,40 @@
+<?php
+// database host
+$db_host   = "localhost:3306";
+
+// database name
+$db_name   = "ecshop";
+
+// database username
+$db_user   = "root";
+
+// database password
+$db_pass   = "123456";
+
+// table prefix
+$prefix    = "main_";
+
+$timezone    = "PRC";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('STORES_PATH','stores');
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+
+define('API_TIME', '2014-10-31 14:05:51');
+
+define('BONUS_MD5_CODE', 'sini2014_elife');
+ 
+
+?>
